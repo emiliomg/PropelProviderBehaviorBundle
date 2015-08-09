e0e111af3c2f978cef14db126424d707b8ed50db
Emilio Markgraf <emilio.markgraf@krankikom.de>
2015-08-09 12:50:10 +0200

    Use new providerCache location and new package key to generate the provider service definition

--- src/DependencyInjection/Compiler/AddProviderBehaviorCompilerPass.php
+++ src/DependencyInjection/Compiler/AddProviderBehaviorCompilerPass.php
@@ -26,30 +26,46 @@ class AddProviderBehaviorCompilerPass implements CompilerPassInterface
         $definition = $container->getDefinition('propel.build_properties');
         $argument = $definition->getArgument(0);
 
+//        Register BaseBehavior
         if (!isset($argument['propel.behavior.providerBase.behavior'])) {
             $argument['propel.behavior.providerBase.behavior'] = 'vendor.emiliomg.propel-provider-behavior.src.ProviderBaseBehavior.ProviderBaseBehavior';
         }
+//        Register FassadeBehavior
         if (!isset($argument['propel.behavior.providerFassade.behavior'])) {
             $argument['propel.behavior.providerFassade.behavior'] = 'vendor.emiliomg.propel-provider-behavior.src.ProviderFassadeBehavior.ProviderFassadeBehavior';
         }
+//        Register both behaviors as defaulf behaviors
         $argumentDefault = isset($argument['propel.behavior.default']) ? $argument['propel.behavior.default'].', ' : '';
         $argumentDefault .= 'providerBase, providerFassade';
         $argument['propel.behavior.default'] = $argumentDefault;
 
+//        Enable cacheFile generation
+        if (!isset($argument['propel.behavior.provider.cachefile'])) {
+            $argument['propel.behavior.provider.cachefile'] = 'true';
+        }
+
         $definition->replaceArgument(0, $argument);
+
 
 //        Check if provider cache exists.
 //        If it does, use it to set the appropriate service definitions
-        $file = $container->getParameter('kernel.root_dir').'/../vendor/emiliomg/propel-provider-behavior/cache/providerCache.json';
+        $file = $container->getParameter('kernel.root_dir').'/propel/providerCache.json';
         if (file_exists($file)) {
             $providers = json_decode(file_get_contents($file), true);
             if (JSON_ERROR_NONE == json_last_error()) {
                 if (is_array($providers)) {
                     foreach ($providers as $provider) {
-                        $providerId = strtolower(str_replace('\\', '_', $provider['namespace'])).
+                        $providerFullName = $provider['namespace'].'\\'.$provider['providerName'];
+                        $providerId = strtolower(
+                            $provider['package'].
                             '.provider.'.
-                            strtolower($provider['modelName']);
-                        $definition = new Definition($provider['providerNamespace']);
+                            strtolower($provider['modelName'])
+                        );
+                        if ('src.' === substr($providerId, 0, 4)) {
+                            $providerId = substr($providerId, 4);
+                        }
+
+                        $definition = new Definition($providerFullName);
                         $container->setDefinition($providerId, $definition);
                     }
                 }
